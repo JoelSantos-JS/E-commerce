d6e6d67d7e43daf2fe634985c18d41ff2d8ee649
Joel Santos <joeltere9@gmail.com>
2023-05-24 12:14:49 -0300

    create quantities and order

---
+++ src/main/java/com/ecommerce/ECommerce/model/WebOrder.java
@@ -0,0 +1,67 @@
+package com.ecommerce.ECommerce.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "web_order")
+public class WebOrder {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private LocalUser user;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address address;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<WebOrdersQuantity> quantities = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalUser getUser() {
+        return user;
+    }
+
+    public void setUser(LocalUser user) {
+        this.user = user;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<WebOrdersQuantity> getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(List<WebOrdersQuantity> quantities) {
+        this.quantities = quantities;
+    }
+
+}
---
+++ src/main/java/com/ecommerce/ECommerce/model/WebOrdersQuantity.java
@@ -0,0 +1,63 @@
+package com.ecommerce.ECommerce.model;
+
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties.Web;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "web_orders_quantity")
+public class WebOrdersQuantity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Integer quantity;
+    @ManyToOne()
+    @JoinColumn(name = "order_id", nullable = false)
+    private WebOrder order;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public WebOrder getOrder() {
+        return order;
+    }
+
+    public void setOrder(WebOrder order) {
+        this.order = order;
+    }
+
+}
